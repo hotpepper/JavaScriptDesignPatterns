95b7b4a8bb5691db3aed2584c0d99df48346a4c3
hotpepper <heyseth@hotmail.com>
2015-02-08 17:43:57 -0200

    adds cat constructor

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ var cat2_name = "Kristen"
 $("#cat1_name").text(cat1_name)
 $("#cat2_name").text(cat2_name)
 
+//count clicks
 var clickCounter = function(cat, cat_label, cat_count) {
 	$(cat).click(function(){
 		cat_count++;
@@ -17,3 +18,15 @@ var clickCounter = function(cat, cat_label, cat_count) {
 
 clickCounter("#cat1", ".cat1_label", cat1_clicks);
 clickCounter("#cat2", ".cat2_label", cat2_clicks);
+
+
+//cat constructor
+var Cat = function (name, img, count) {
+  this.name = name;
+  this.img = img;
+  this.count = count;
+  console.log('Cat instantiated');
+};
+
+var cat1 = new Cat('Seth');
+var cat2 = new Cat('Kristen');

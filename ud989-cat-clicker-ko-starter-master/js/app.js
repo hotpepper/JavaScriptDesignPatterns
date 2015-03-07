062ee47dc83efef35f493322090793b7bbac077a
hotpepper <heyseth@hotmail.com>
2015-03-07 19:17:13 -0200

    adds knockout computed observables levels

--- ud989-cat-clicker-ko-starter-master/js/app.js
+++ ud989-cat-clicker-ko-starter-master/js/app.js
@@ -1,8 +1,26 @@
+
+
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("Tabby");
 	this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
 	this.imgAttribution = ko.observable("https://flicker.com");
+
+	//levels
+	this.level = ko.computed(function() {
+		var level = "Newborn";
+		if (this.clickCount()<10){
+			level = "Newborn"
+		} else if (this.clickCount()>9 && this.clickCount()<13){
+			level = "Infant"
+		} else if (this.clickCount()>12 && this.clickCount()<32) {
+			level = "Teen"
+		} else {
+			level ="Old man"
+		}
+        return level
+    }, this);
+
 
 	this.incrementCounter = function(){
 		this.clickCount( this.clickCount() + 1 );
